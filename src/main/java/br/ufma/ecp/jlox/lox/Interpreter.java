4947d4fcdc37f90c8db86e5e4753db890f7320d9
Sergio Costa <sergio.costa@ufma.br>
2021-05-19 14:37:39 -0300

    add if statement

--- src/main/java/br/ufma/ecp/jlox/lox/Interpreter.java
+++ src/main/java/br/ufma/ecp/jlox/lox/Interpreter.java
@@ -43,6 +43,29 @@ class Interpreter implements Expr.Visitor<Object>,
 
     }
 
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
         private String stringify(Object object) {
         if (object == null) return "nil";
 
